d56679defadbf1145084ea493f7712d010b96012
yurifidelis <129562565+yurifidelis@users.noreply.github.com>
2023-05-17 20:15:01 -0300

    Create Questão 11.py

---
+++ Questão 11.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+num3 = float(input("Digite um número real: "))
+
+resultado1 = (2 * num1) * (num2 / 2)
+resultado2 = (3 * num1) + num3
+resultado3 = num3 ** 3
+
+print("O produto do dobro do primeiro com metade do segundo é:", resultado1)
+print("A soma do triplo do primeiro com o terceiro é:", resultado2)
+print("O terceiro elevado ao cubo é:", resultado3)
