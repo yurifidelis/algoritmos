99eef987b18948a22f45cecdb93b8340ba87c218
yurifidelis <129562565+yurifidelis@users.noreply.github.com>
2023-05-17 20:11:31 -0300

    Questão 8.py

--- Questão 8.py
+++ Questão 8.py
@@ -1,1 +1,7 @@
+valor_hora = float(input("Digite o valor do salário por hora: "))
+horas_trabalhadas = float(input("Digite o número de horas trabalhadas no mês: "))
+
+salario = valor_hora * horas_trabalhadas
+
+print("O total do seu salário no mês é:", salario)
 
