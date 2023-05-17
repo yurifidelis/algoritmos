39033cd311bc55672c6b3b986fa111ee8831c772
yurifidelis <129562565+yurifidelis@users.noreply.github.com>
2023-05-17 20:10:23 -0300

    Create Questão 7.py

---
+++ Questão 7.py
@@ -0,0 +1,7 @@
+lado = float(input("Digite o valor do lado do quadrado: "))
+
+area = lado ** 2
+dobro_area = 2 * area
+
+print("A área do quadrado é:", area)
+print("O dobro da área do quadrado é:", dobro_area)
